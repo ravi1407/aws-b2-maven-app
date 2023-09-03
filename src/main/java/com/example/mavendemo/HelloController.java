d407020ff1a1e48016aead6fb25ff3228e84726a
ravi1407 <momdalapu@gmail.com>
2023-09-03 23:29:31 +0530

    added file

---
+++ src/main/java/com/example/mavendemo/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.mavendemo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping('/')
+	public String index() {
+        return "Greetings from Python life Devops class today";
+	}
+
+}
